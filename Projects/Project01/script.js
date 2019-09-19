d0721d55c9c0e9ec30355033e42af7f0407f50bb
lennye Shumate <LShuamte039@west-mec.org>
2019-09-18 18:20:31 -0700

    update 9/18

--- Projects/Project01/script.js
+++ Projects/Project01/script.js
@@ -7,3 +7,74 @@
 */
 
 "use strict";
+
+/* global variables */
+var httpRequest = false;
+var entry = "^IXIC";
+
+function getRequestObject(){
+    try {
+        httpRequest = new XMLHttpRequest();
+    }
+    catch (requestError) {
+        return false;
+    }
+    alert(httpRequest);
+    return httpRequest;
+    
+    
+    
+}
+if (window.addEventListener) {
+    window.addEventListener("load", getRequestObject, false);
+} 
+else if (window.attachEvent) {
+    window.attachEvent("onload", getRequestObject);
+}
+
+function stopSubmission(evt) {
+    alert("stopSubmission()");
+    if (evt.preventDefault) {
+        evt.preventDefault();
+    }
+    else {
+        evt.returnValue = false;
+    }
+}
+
+
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", getRequestObject, false);
+} 
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", getRequestObject);
+}
+
+
+function getQuote() {
+    alert("getQuote()");
+    if (document.getElementsByTagName("input")[0].value) {
+        entry = document.getElementsByTagName("input")[0].value;
+    }
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+}
+var form = document.getElementsByTagName("form")[0];
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", getQuote, false);
+} 
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", getQuote);
+}
+ 
+function getQuote(){
+    httpRequest.abort();
+    httpRequest.open("get", "StockCheck.php?t=" + entry, true);
+    httpRequest.send(null);
+}
