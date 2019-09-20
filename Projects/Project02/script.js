e23fdab065de5904a1c2ebb0f28b7aefccb08ee2
lennye Shumate <LShuamte039@west-mec.org>
2019-09-19 18:14:34 -0700

    update 9/19

--- Projects/Project02/script.js
+++ Projects/Project02/script.js
@@ -8,3 +8,27 @@
 
 "use strict";
 
+var httpRequest = false;
+
+
+
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false);
+} 
+else if (zip.attachEvent) {
+    zip.attachEvent("onkeyup", checkInput);
+}
+
+
+function alert(){
+    var zip = document.getElementById("zip").value;
+    if (zip.length === 5) {
+        getLocation()
+    }
+    else {
+        document.getElementById("city").value = "";
+        document.getElementById("state").value = "";
+    }
+}
+
--- Projects/Project_Lab/www/script.js
+++ Projects/Project_Lab/www/script.js
@@ -22,8 +22,6 @@ function getRequestObject(){
     alert(httpRequest);
     return httpRequest;
     
-    
-    
 }
 if (window.addEventListener) {
     window.addEventListener("load", getRequestObject, false);
@@ -77,4 +75,54 @@ function getQuote(){
     httpRequest.abort();
     httpRequest.open("get", "StockCheck.php?t=" + entry, true);
     httpRequest.send(null);
+}
+
+
+
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var stockResults = httpRequest.responseText;
+        console.log(stockResults);
+     }
+     httpRequest.onreadystatechange = displayData; 
+}
+
+
+
+
+
+for (var i = stockItems.length - 1; i >= 0; i--) {
+    if (stockItems[i] === "") {
+        stockItems.splice(i, 1);
+        var stockItems = stockResults.split(/,|\"/);
+    }
+    console.log(stockItems);
+}
+
+
+document.getElementById("ticker").innerHTML = stockItems[0];
+
+document.getElementById("openingPrice").innerHTML = stockItems[6];
+document.getElementById("lastTrade").innerHTML = stockItems[1];
+document.getElementById("lastTradeDT").innerHTML = stockItems[2] + ", " + stockItems[3];
+document.getElementById("change").innerHTML = stockItems[4];
+document.getElementById("range").innerHTML = (stockItems[8] * 1).toFixed(2) + " &ndash; " + (stockItems[7] * 1).toFixed(2);
+document.getElementById("volume").innerHTML = (stockItems[9] * 1).toLocaleString();
+
+function formatTable() {
+    var rows = document.getElementsByTagName("tr");
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].style.background = "#9FE098";
+    }
+}
+
+if (form.addEventListener) {
+    form.addEventListener("submit", stopSubmission, false);
+    window.addEventListener("load", formatTable, false);
+    window.addEventListener("load", getQuote, false);
+} 
+else if (form.attachEvent) {
+    form.attachEvent("onsubmit", stopSubmission);
+    window.attachEvent("onload", formatTable);
+    window.attachEvent("onload", getQuote);
 }
